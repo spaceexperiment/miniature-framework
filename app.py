fe034afc62ac2dc70bc4d0ba777262a1aec83a23
jibreel <jibbo88@gmail.com>
2014-07-29 17:57:20 +0300

    set and get cookie url

--- app.py
+++ app.py
@@ -3,10 +3,9 @@ from wsgi import App
 
 app = App()
 
-
 @app.route('/')
 def home():
-    print('test')
+    print(app.request.get)
     return 'Home page'
 
 
@@ -14,23 +13,31 @@ def home():
 def post():
     return app.request.json
 
+
 @app.route('/form', methods=['POST'])
 def form():
     print(app.request.files)
     return app.request.data
+
 
 @app.route('/data', methods=['POST'])
 def data():
     # text data, not form or json
     return app.request.data
 
-@app.route('/cookie')
-def cookie():
+
+@app.route('/setcookie')
+def set_cookie():
     # set headers
     app.headers = {'Content-Type': 'application/json',
-                   'Set-Cookie': 'user=me; path=/;'}
+                   'Set-Cookie': 'user=marv; pass=$@#!; path=/;'}
 
-    return app.request.headers.get('HTTP_COOKIE', 'No cookie')
+    return 'cookies set'
+
+
+@app.route('/getcookie')
+def get_cookie():
+    return app.request.headers.get('HTTP_COOKIE', 'No cookies :(')
 
 
 if __name__ == '__main__':
