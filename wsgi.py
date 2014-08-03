af64cfd7d48d08e6739a9ce6bfa94ffe92a24b05
jibreel <jibbo88@gmail.com>
2014-08-03 17:56:47 +0300

    moved render logic ro Response, lots of cleanup, and removed json

--- wsgi.py
+++ wsgi.py
@@ -14,13 +14,10 @@ class Request(object):
     """
 
     def __init__(self, environ):
+        self.environ = environ
         self.headers = self._parse_headers(environ)
-        self.get = self._parse_query(environ)
-        self.data = {}
-        self.files = {}
-        self.json = None
-        self.set_headers = {}
-        self._parse_data(environ)
+        self.query = self._parse_query(environ)
+        self.data = self._parse_data(environ)
 
     def _parse_query(self, environ):
         query = cgi.parse_qs(environ['QUERY_STRING'])
@@ -28,8 +25,7 @@ class Request(object):
 
     def _parse_headers(self, environ):
         length = environ.get('CONTENT_LENGTH', 0)
-        length = 0 if not length else int(length)
-        headers = {'CONTENT_LENGTH': length}
+        headers = {'CONTENT_LENGTH': 0 if not length else int(length)}
 
         wanted_headers = ['REQUEST_METHOD', 'PATH_INFO', 'REMOTE_ADDR',
                           'REMOTE_HOST', 'CONTENT_TYPE']
@@ -41,6 +37,7 @@ class Request(object):
 
     def _parse_data(self, environ):
         content_type = environ['CONTENT_TYPE'].lower()
+        data = {}
         if 'form' in content_type:
             env_data = cgi.FieldStorage(environ['wsgi.input'],
                                         environ=environ)
@@ -48,127 +45,99 @@ class Request(object):
                 # filter out url queries
                 if not isinstance(k, cgi.MiniFieldStorage):
                     if k.filename:
-                        self.files[k.name] = k.file
+                        data[k.name] = k.file
                     else:
-                        self.data[k.name] = k.value
-
-        elif 'json' in content_type:
-            length = self.headers['CONTENT_LENGTH']
-            data = environ['wsgi.input'].read(length)
-            data = data.decode('utf-8').strip('\'')
-            try:
-                self.json = json.loads(data)
-            except ValueError:
-                # todo return error invalid json
-                print('error')
-                pass
-
+                        data[k.name] = k.value
+            return data
         else:
             length = self.headers['CONTENT_LENGTH']
-            self.data = environ['wsgi.input'].read(length)
+            return environ['wsgi.input'].read(length)
 
 
 class Response(object):
     """
-    Response object is responsable for setting the headers,
-     cookie and response code.
-    then initiating the make_response and returning the view data
-    :params request, the request object, we input this AFTER we render
-        the view, so that we can implement the user specified header...
+    Response object is responsable for initiating the make_response and returning the view data
     :params code, the status code
     :params data, the raw data rendered from the view
 
     """
-    def __init__(self, request, code=200, data=''):
-        headers = request.set_headers
-        # todo set_cookie
+    def __init__(self, make_response, code=200, data=''):
+        # view can return str or str and a dict of headers
+        if isinstance(data, tuple):
+            self.data = data[0]
+            headers = data[1]
+        else:
+            self.data = data
+            headers = {}
 
         if 'content-type' not in map(lambda x: x.lower(), headers):
             headers['Content-Type'] = 'text/html'
 
         self.headers = [(k, v) for k, v in headers.items()]
-        self.request = request
-        self.data = data
         self.code = code
+        self.make_response = make_response
 
     def render(self):
         resp_code = '{} {}'.format(self.code, httplib.responses[self.code])
 
-        def make_response(code):
-            self.request.make_response(code, self.headers)
-
         if str(self.code)[0] in ['4', '5']:
-            make_response(resp_code)
-            return '<h1>{}</h1>'.format(resp_code).encode('utf-8')
+            self.make_response(resp_code, self.headers)
+            yield resp_code.encode('utf-8')
 
-        make_response(resp_code)
-        return self.data
+        try:
+            data = bytes(self.data)
+        except Exception:
+            data = str(self.data).encode('utf-8')
+
+        self.make_response(resp_code, self.headers)
+        yield data
 
 
 class App(object):
 
     def __init__(self):
         self.routes = {}
-        self.request = None
-
-    @property
-    def headers(self):
-        return self.request.headers
-
-    @headers.setter
-    def headers(self, value):
-        self.request.set_headers = value
 
     def route(self, url, methods=['GET']):
-        """
-        Example usage
-
-        @app.route('/home')
-        def home():
-            return 'Welcome home'
-        """
 
         def decorate(f):
 
             @wraps(f)
             def wrapper(*args, **kwargs):
-                try:
-                    results = bytes(f(*args, **kwargs))
-                except Exception:
-                    results = str(f(*args, **kwargs)).encode('utf-8')
-                return results
+                return f(*args, **kwargs)
 
-            self.routes[url] = {'methods': methods, 'render': wrapper}
+            self.routes[url] = {'methods': methods, 'func': wrapper}
 
             return wrapper
         return decorate
 
-    def path_dispatch(self):
-        path = self.request.headers['PATH_INFO']
-        self.method = self.request.headers['REQUEST_METHOD']
+    def path_dispatch(self, request, make_response):
+        path = request.headers['PATH_INFO']
+        method = request.headers['REQUEST_METHOD']
         view = self.routes.get(path)
         if not view:
-            response = Response(self.request, 404)
-        elif self.method not in view['methods']:
-            response = Response(self.request, 405)
+            response = Response(make_response, 404)
+        elif method not in view['methods']:
+            response = Response(make_response, 405)
         else:
-            data = view['render']()
-            response = Response(self.request, data=data)
+            data = view['func'](request)
+            response = Response(make_response, data=data)
 
         return response
 
-    def dispatch_request(self, environ):
-        self.request = Request(environ)
-        self.response = self.path_dispatch()
+    def dispatch_request(self, environ, make_response):
+        request = Request(environ)
+        response = self.path_dispatch(request, make_response)
+        return response
 
     def __call__(self, environ, make_response):
         """The actual wsgi app"""
-        self.dispatch_request(environ)
-        self.request.make_response = make_response
-        yield self.response.render()
+        resp = self.dispatch_request(environ, make_response)
+        return resp.render()
+
 
     def run(self, host='', port=8080):
         """ server """
         httpd = make_server(host, port, self)
-        print('Serving on localhost:8080')
+        print('Serving on {host}:{port}'.format(host=host, port=port))
         httpd.serve_forever()
